6f62853a735011e32fe167601256792e95eac6ac
Robert R <rarDevelopment@users.noreply.github.com>
2024-02-24 11:39:51 -0500

    update file name to use

--- now-page-finder.js
+++ now-page-finder.js
@@ -1,53 +1,55 @@
 import fs from 'fs';
 import 'dotenv/config';
 
+const apiUrl = process.env.API_URL;
+
 const scriptsToRun = [
   {
     name: 'updateRecentGames',
-    urlToUse: `https://rardk64-bot-api.com/api/now/backloggd/reviews?limit=5`,
-    fileName: `${process.env.DIRECTORYPATH}recent-games.json`,
+    urlToUse: `${apiUrl}backloggd/reviews?limit=5`,
+    fileName: `${process.env.DIRECTORYPATH}now-recent-games.json`,
     allowEmpty: false,
   },
   {
     name: 'updateCurrentGames',
-    urlToUse: `https://rardk64-bot-api.com/api/now/backloggd/current?limit=5`,
-    fileName: `${process.env.DIRECTORYPATH}current-games.json`,
+    urlToUse: `${apiUrl}backloggd/current?limit=5`,
+    fileName: `${process.env.DIRECTORYPATH}now-current-games.json`,
     allowEmpty: true,
   },
   {
     name: 'updateRecentMovies',
-    urlToUse: `https://rardk64-bot-api.com/api/now/letterboxd?limit=5`,
-    fileName: `${process.env.DIRECTORYPATH}recent-movies.json`,
+    urlToUse: `${apiUrl}letterboxd?limit=5`,
+    fileName: `${process.env.DIRECTORYPATH}now-recent-movies.json`,
     allowEmpty: false,
   },
   {
     name: 'updateCurrentTv',
-    urlToUse: `https://rardk64-bot-api.com/api/now/serializd/currentlywatching?limit=5`,
-    fileName: `${process.env.DIRECTORYPATH}current-tv.json`,
+    urlToUse: `${apiUrl}serializd/currentlywatching?limit=5`,
+    fileName: `${process.env.DIRECTORYPATH}now-current-tv.json`,
     allowEmpty: true,
   },
   {
     name: 'updateCurrentBooks',
-    urlToUse: `https://rardk64-bot-api.com/api/now/goodreads/currently-reading?limit=5`,
-    fileName: `${process.env.DIRECTORYPATH}current-books.json`,
+    urlToUse: `${apiUrl}goodreads/currently-reading?limit=5`,
+    fileName: `${process.env.DIRECTORYPATH}now-current-books.json`,
     allowEmpty: true,
   },
   {
     name: 'updateFinishedBooks',
-    urlToUse: `https://rardk64-bot-api.com/api/now/goodreads/finished?limit=5`,
-    fileName: `${process.env.DIRECTORYPATH}recent-books.json`,
+    urlToUse: `${apiUrl}goodreads/finished?limit=5`,
+    fileName: `${process.env.DIRECTORYPATH}now-recent-books.json`,
     allowEmpty: false,
   },
   {
     name: 'Update Top Albums (This Month)',
-    urlToUse: `https://rardk64-bot-api.com/api/now/lastfm/topalbums?limit=5&period=1month`,
-    fileName: `${process.env.DIRECTORYPATH}top-albums.json`,
+    urlToUse: `${apiUrl}lastfm/topalbums?limit=5&period=1month`,
+    fileName: `${process.env.DIRECTORYPATH}now-top-albums.json`,
     allowEmpty: false,
   },
   {
     name: 'Update Top Artists (This Month)',
-    urlToUse: `https://rardk64-bot-api.com/api/now/lastfm/topartists?limit=5&period=1month`,
-    fileName: `${process.env.DIRECTORYPATH}top-artists.json`,
+    urlToUse: `${apiUrl}lastfm/topartists?limit=5&period=1month`,
+    fileName: `${process.env.DIRECTORYPATH}now-top-artists.json`,
     allowEmpty: false,
   },
 ];
@@ -68,16 +70,3 @@ async function updateNowJson(scriptName, url, fileName, allowEmpty) {
 scriptsToRun.forEach((script) => {
   updateNowJson(script.name, script.urlToUse, script.fileName, script.allowEmpty);
 });
-
-// async function updateRecentGames(url, fileName) {
-//   try {
-//     const response = await fetch(url);
-//     const recentGames = await response.json();
-//     if (recentGames.length === 0) {
-//       return;
-//     }
-//     fs.writeFileSync(fileName, JSON.stringify(recentGames, '', 2));
-//   } catch (error) {
-//     console.error('unable to fetch games data', error);
-//   }
-// }
