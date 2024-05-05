ad189175365bc8eed7cff8b81ecd97bab627cd6d
Robert R <rarDevelopment@users.noreply.github.com>
2024-05-04 21:15:12 -0400

    don't retrieve images for sets that already have them

--- lego-set-meta-updater.js
+++ lego-set-meta-updater.js
@@ -4,16 +4,28 @@ import 'dotenv/config';
 const apiUrl = process.env.API_URL;
 
 const legoSetsUrl = `${apiUrl}json/lego-sets`;
+const legoSetsWithImagesUrl = `${apiUrl}json/lego-sets-with-images`;
 const legoImageUrl = `${apiUrl}lego/image?url={{SET_URL}}`;
 const fileName = `${process.env.DIRECTORYPATH}lego-sets-images.json`;
 
 try {
   const legoSetsResponse = await fetch(legoSetsUrl);
+  const legoSetsWithImagesResponse = await fetch(legoSetsWithImagesUrl);
+
   const legoSets = await legoSetsResponse.json();
+  const existingLegoSetsWithImages = await legoSetsWithImagesResponse.json();
+
   let legoSetsWithImages = [];
   if (legoSets && legoSets.length > 0) {
     await Promise.all(
       legoSets.map(async (set) => {
+        var existingSet = existingLegoSetsWithImages.find(
+          (existingSet) => existingSet.url === set.url
+        );
+        if (existingSet && existingSet.imageUrl && existingSet.previewUrl) {
+          legoSetsWithImages.push(existingSet);
+          return;
+        }
         const response = await fetch(legoImageUrl.replace('{{SET_URL}}', set.url));
         const imageUrlData = await response.json();
         set.imageUrl = imageUrlData.imageUrl;
