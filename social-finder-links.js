16f6439bf8b3dca9d2e4e831b53dfdcb85a7bd89
Robert R <rarDevelopment@users.noreply.github.com>
2024-07-15 00:12:04 -0400

    change link script to use hashtag

--- social-finder-links.js
+++ social-finder-links.js
@@ -1,10 +1,11 @@
-import extractUrls from 'extract-urls';
 import fs from 'fs';
 import 'dotenv/config';
 
 //modified from: https://github.com/rknightuk/api/blob/117aade2783beeb09686e9f7e7d3775facf06722/services/discussion.js
 
-const urlRegexPattern = /http(s)?:\/\/(www.)?rardk\.com\/links/g;
+const hashtag = '#rardklink';
+const hashtagRegexPattern = /#rardklink\d+/g;
+const mastodonHashtagRegexPattern = /rardklink\d+/g;
 const mastodonUrl = `https://mastodon.social/api/v1/accounts/425168/statuses?exclude_replies=true&limit=50&exclude_reblogs=true`;
 const blueskyUrl = `https://bsky.social/xrpc/com.atproto.repo.listRecords?collection=app.bsky.feed.post&repo=rardk64.com`;
 const fileName = `${process.env.DIRECTORYPATH}discussion-links-posts.json`;
@@ -15,25 +16,26 @@ async function run() {
     const res = await fetch(mastodonUrl);
     const mastodonPosts = await res.json();
     mastodonPosts.forEach((t) => {
-      const urls = (extractUrls(t.content) || []).filter((url) => url.match(urlRegexPattern));
-      const isSyndicate = urls.some((url) => url.match(urlRegexPattern));
+      const foundHashtag = t.tags.find((tag) =>
+        //mastodon doesn't include the # in the tag name in the separated tags objects
+        tag.name.match(mastodonHashtagRegexPattern)
+      );
+      const isSyndicate = foundHashtag ? true : false;
       if (isSyndicate) {
-        urls.forEach((url) => {
-          let path = new URL(url).pathname;
-          if (!discussion[path]) {
-            discussion[path] = {
-              mastodon: [],
-              bluesky: [],
-            };
-          }
-          const existingPost = discussion[path].mastodon.find((e) => e.id === t.id);
-          if (!existingPost) {
-            discussion[path].mastodon.push(t);
-          } else {
-            const index = discussion[path].mastodon.indexOf(existingPost);
-            discussion[path].mastodon[index] = t;
-          }
-        });
+        let path = foundHashtag.name.replace(hashtag.replace('#', ''), '');
+        if (!discussion[path]) {
+          discussion[path] = {
+            mastodon: [],
+            bluesky: [],
+          };
+        }
+        const existingPost = discussion[path].mastodon.find((e) => e.id === t.id);
+        if (!existingPost) {
+          discussion[path].mastodon.push(t);
+        } else {
+          const index = discussion[path].mastodon.indexOf(existingPost);
+          discussion[path].mastodon[index] = t;
+        }
       }
     });
   } catch (error) {
@@ -43,46 +45,28 @@ async function run() {
     const res = await fetch(blueskyUrl);
     const blueskyResponse = await res.json();
     blueskyResponse.records.forEach((b) => {
-      const urls = getLinksFromBlueSkyPost(b);
-      const isSyndicate = urls.some((url) => url.match(urlRegexPattern));
+      const isSyndicate = b.value.text.match(hashtagRegexPattern);
       if (isSyndicate) {
-        urls.forEach((url) => {
-          let path = new URL(url).pathname;
-          if (!discussion[path])
-            discussion[path] = {
-              mastodon: [],
-              bluesky: [],
-            };
-          const existingPost = discussion[path].bluesky.find((e) => e.cid === b.cid);
-          if (!existingPost) {
-            discussion[path].bluesky.push(b);
-          } else {
-            const index = discussion[path].bluesky.indexOf(existingPost);
-            discussion[path].bluesky[index] = b;
-          }
-        });
+        const indexOfHashtag = b.value.text.indexOf(hashtag);
+        let path = b.value.text.slice(indexOfHashtag).replace(hashtag, '');
+        if (!discussion[path])
+          discussion[path] = {
+            mastodon: [],
+            bluesky: [],
+          };
+        const existingPost = discussion[path].bluesky.find((e) => e.cid === b.cid);
+        if (!existingPost) {
+          discussion[path].bluesky.push(b);
+        } else {
+          const index = discussion[path].bluesky.indexOf(existingPost);
+          discussion[path].bluesky[index] = b;
+        }
       }
     });
   } catch (error) {
     console.error('unable to fetch bluesky data', error);
   }
   fs.writeFileSync(fileName, JSON.stringify(discussion, '', 2));
-}
-
-function getLinksFromBlueSkyPost(post) {
-  const facets = post.value.facets;
-  if (!facets) {
-    return [];
-  }
-  let links = [];
-  facets.forEach((f) =>
-    f.features.forEach((feat) => {
-      if (feat.$type === 'app.bsky.richtext.facet#link') {
-        links.push(feat.uri);
-      }
-    })
-  );
-  return links;
 }
 
 run();
